3bbca89b0f572f8fd352cdd256b29c337d9497ea
Garvin <gtan36@student.monash.edu>
2021-12-17 12:15:38 +1100

    Initial home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/page/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,76 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cash Counting',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('End of Day Count'),
-      ),
-      body: Center(
-          child: ElevatedButton(
-        child: const Text('Begin'),
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const SafeCountPage()));
-        },
-      )),
-    );
-  }
-}
-
-class SafeCountPage extends StatefulWidget {
-  const SafeCountPage({Key? key}) : super(key: key);
-
-  @override
-  State<SafeCountPage> createState() => _SafeCountState();
-}
-
-class _SafeCountState extends State<SafeCountPage> {
-  String dropdownValue = "1";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Safe Count")),
-        body: Center(
-            child: Card(
-          child: Row(
-            children: <Widget>[
-              const Text("50c"),
-              DropdownButton<String>(
-                value: dropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                items: <String>["1", "2", "3"]
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-        )));
   }
 }
---
+++ lib/page/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const String route = '/';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+          body: Center(
+        child: Column(
+          children: [
+            const Expanded(child: Text('Cash Counting')),
+            ElevatedButton(onPressed: () {}, child: const Text('Start'))
+          ],
+        ),
+      ));
+
+  //  void handleClick() {
+  //    Navigator.pushNamed(context, SafePage.route);
+  //  }
+
+}
