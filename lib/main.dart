fc80d6a1ee4f718c8eec99c377a67637d41e5ce4
Garvin <gtan36@student.monash.edu>
2021-12-22 21:59:00 +1100

    Added multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:app/page/home.dart';
+import 'package:app/provider/safe_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => SafeModel())],
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
